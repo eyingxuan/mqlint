ff112bdca064872437a5ba40c3ac00df23baf897
Davis Haupt <davis.haupt@gmail.com>
2020-11-16 16:47:41 -0500

    initial commit

---
+++ types.hs
@@ -0,0 +1,56 @@
+type FieldPath = String
+
+data BSON = Dbl Double
+  | Str String
+  | Object [(String, BSON)]
+  | Array [BSON]
+  | ObjectId String
+  | Null
+  | Intgr Int
+  | Date Int
+
+data BSONType = TDbl
+  | TStr
+  | TObject [(String, BSONType)]
+  | TArray BSONType
+  | TObjectId
+  | TNull
+  | TIntgr
+  | TDate
+
+data Op = Add
+  | Abs
+  | Ceil
+  | Floor
+  | Avg
+  | Min
+  | Max
+  | Eq
+
+data Accumulator = Avg
+  | First
+  | Last
+  | Min
+  | Max
+
+data Expression = FP FieldPath
+  | Lit BSON
+  | Obj [(String, Expression)]
+  | Application Op [Expression]
+
+
+data ProjectField = Inclusion Boolean | NewField Expression
+
+data Stage = Match Expression
+  | Facet [(String, AST)]
+  | Lookup (String, FieldPath, FieldPath, String)
+  | Project [(String, ProjectField)]
+  | Unwind FieldPath
+  | Group (Expression, [(String, Accumulator, Expression)])
+
+newtype AST = Pipeline [Stage]
+
+{--
+  State monad with every field and its type
+
+--}
