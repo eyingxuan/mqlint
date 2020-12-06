148101833d6fc8064659c9265aac98c462fe062e
Ying Xuan <engyingxuan@gmail.com>
2020-12-06 13:20:26 -0500

    change to nested exclusion test

--- test/TypecheckerTests.hs
+++ test/TypecheckerTests.hs
@@ -12,14 +12,26 @@ import Types (AST (..), BSONType (..), Expression (..), Index (..), SchemaTy (..
 typecheckerTests :: Test
 typecheckerTests = TestList [testSimpleLookup, testSimpleProject]
 
+s1 :: Map.Map String BSONType
+s1 = Map.fromList [("x", TStr), ("y", TStr)]
+
+s2 :: Map.Map String BSONType
+s2 = Map.fromList [("x", TStr), ("z", TNumber)]
+
+s3 :: Map.Map String BSONType
+s3 = Map.fromList [("x", TObject s1)]
+
+s4 :: Map.Map String BSONType
+s4 = Map.fromList [("x", TObject s2)]
+
 m1 :: SchemaTy
-m1 = S (Set.fromList [Map.fromList [("x", TStr), ("y", TStr)]])
+m1 = S (Set.fromList [s1])
 
 m2 :: SchemaTy
-m2 = S (Set.fromList [Map.fromList [("x", TStr), ("z", TNumber)]])
+m2 = S (Set.fromList [s2])
 
 m3 :: SchemaTy
-m3 = S (Set.fromList [Map.fromList [("x", TStr), ("z", TNumber), ("b", TNumber)], Map.fromList [("x", TStr), ("z", TNumber), ("a", TNumber)]])
+m3 = S (Set.fromList [s3, s4])
 
 db1 :: Map.Map String SchemaTy
 db1 = Map.fromList [("col1", m1), ("col2", m2)]
@@ -73,11 +85,18 @@ testSimpleProject =
           ( runExceptT
               ( runReaderT
                   ( typecheck
-                      (Pipeline [Project (Map.fromList [("z", Inclusion False)])])
+                      (Pipeline [Project (Map.fromList [("x", EObject (Map.fromList [("x", Inclusion False)]))])])
                       m3
                   )
                   db1
               )
           )
-          ~?= Right (S (Set.fromList [Map.fromList [("a", TNumber), ("x", TStr)], Map.fromList [("b", TNumber), ("x", TStr)]]))
+          ~?= Right
+            ( S
+                ( Set.fromList
+                    [ Map.fromList [("x", TObject (Map.fromList [("y", TStr)]))],
+                      Map.fromList [("x", TObject (Map.fromList [("z", TNumber)]))]
+                    ]
+                )
+            )
       ]
