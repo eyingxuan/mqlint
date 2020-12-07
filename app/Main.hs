3653a99e3db4cafacabc47ae1be8c35f1a42a8ce
Davis Haupt <davis.haupt@gmail.com>
2020-12-07 14:22:42 -0400

    more changes

--- app/Main.hs
+++ app/Main.hs
@@ -3,19 +3,27 @@ module Main where
 import MqlParser (getPipelineFromFile)
 import SchemaParser (getContextFromFile)
 import System.Environment (getArgs)
+import qualified Data.Map as Map
+import Typechecker (runTypechecker)
 
 ignore :: a -> ()
 ignore _ = ()
+
+
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    schemaFile : pipelineFile : _ -> do
+    schemaFile : pipelineFile : collectionName : _ -> do
       ctx <- getContextFromFile schemaFile
       pipe <- getPipelineFromFile pipelineFile
       case (ctx, pipe) of
-        (Right context, Right pipeline) -> print (show context ++ "\n---\n" ++ show pipeline)
+        (Right context, Right pipeline) -> 
+          case Map.lookup collectionName context of
+            -- Just schema -> print pipeline
+            Just schema -> print $ runTypechecker pipeline schema context
+            Nothing -> print (show context ++ "\n---\n" ++ show pipeline)
         (Left cerror, Left perror) -> print (cerror ++ "\n---\n" ++ perror)
         (Left cerror, _) -> print cerror
         (_, Left perror) -> print perror
