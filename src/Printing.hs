796e705a847bf4fc79eae86655a8202ffa3a8e28
Davis Haupt <davis.haupt@gmail.com>
2020-12-09 00:43:38 -0400

    pretty printer for expressions done. TODO stages and AST.

--- src/Printing.hs
+++ src/Printing.hs
@@ -1,11 +1,22 @@
 module Printing (PP (..), indented, oneLine) where
 
+import Data.List (intersperse)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Text.PrettyPrint (Doc)
 import qualified Text.PrettyPrint as PP
-import Types (BSONType (..), SchemaTy (..))
+import Types (
+  BSONType (..),
+  SchemaTy (..),
+  BSON (..),
+  Op (..),
+  Accumulator (..),
+  Expression (..),
+  Stage (..),
+  AST (..),
+  Index (..)
+  )
 
 -- import Data.Sequence (mapWithIndex)
 
@@ -56,7 +67,7 @@ arrayify = wrap "[" "]"
 keyvalify :: String -> Doc -> Doc
 keyvalify k v = PP.text "\"" <> PP.text k <> (PP.text "\":" PP.<+> v)
 
-ppo :: Map String BSONType -> Doc
+ppo :: PP a => Map String a -> Doc
 ppo m = objectify $ map (\(k, v) -> keyvalify k (pp v)) (Map.toList m)
 
 ppt :: String -> Doc
@@ -82,6 +93,50 @@ instance PP SchemaTy where
   pp (S l) = case Set.toList l of
     [ty] -> pp $ TObject ty
     l -> arrayify (map (pp . TObject) l)
+
+instance PP BSON where
+  pp (Number n) = PP.double n
+  pp (Str s) = PP.text s
+  pp (ObjectId s) = PP.text s
+  pp Null = PP.text "null"
+  pp (Date d) = PP.int d
+  pp (Boolean True) = PP.text "true"
+  pp (Boolean False) = PP.text "false"
+  pp (Array arr) = arrayify (pp <$> arr)
+  pp (Object obj) = ppo obj
+
+instance PP Op where
+  pp Add = PP.text "$add"
+  pp Abs = PP.text "$abs"
+  pp Ceil = PP.text "$ceil"
+  pp Floor = PP.text "$floor"
+  pp Avg = PP.text "$avg"
+  pp Min = PP.text "$min"
+  pp Max = PP.text "$max"
+  pp Eq = PP.text "$eq"
+
+instance PP Accumulator where
+  pp AAvg = PP.text "$avg"
+  pp First = PP.text "$first"
+  pp Last = PP.text "$last"
+  pp AMin = PP.text "$min"
+  pp AMax = PP.text "$max"
+  pp Push = PP.text "$push"
+  pp Sum = PP.text "$sum"
+
+instance PP Index where
+  pp ArrayIndex = PP.text "0"
+  pp (ObjectIndex s) = PP.text s
+
+instance PP Expression where
+  pp (FP fp) = foldMap id (intersperse (PP.text ".") (pp <$> fp))
+  pp (Inclusion True) = PP.text "1"
+  pp (Inclusion False) = PP.text "0"
+  pp (Lit bson) = pp bson
+  pp (EArray arr) = arrayify (pp <$> arr)
+  pp (EObject obj) = ppo obj
+  pp (Application op exprs) = ppo (Map.fromList [(PP.render (pp op), EArray exprs)])
+
 
 oneLine :: PP a => a -> String
 oneLine = PP.renderStyle (PP.style {PP.mode = PP.OneLineMode}) . pp
